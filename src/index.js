4bd54aaec8c62999a361fc4f2f550417048849a6
Jena Stewart <66694416+jenastewart@users.noreply.github.com>
2024-09-17 16:09:15 -0700

    Added AI APT integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 75,
+    cursor: null,
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "The poem will go here",
-    autoStart: true,
-    delay: "natural",
-    cursor: null,
-  });
+  let userDirectionsInput = document.querySelector("#user-directions");
+  let apiKey = "a17bt048aac153ed9acb6efaf1a6aobf";
+  let prompt = `User directions : Generate a poem about ${userDirectionsInput.value}`;
+  let context =
+    "You are a funny poem expert and love to write short poems. Please generate a 4 line poem and separate each line with a <br />. Make sure to ollow the user directions. Do not include a title. Sign the poem with 'SheCodes AI' using a <strong> element.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
