01b20fc082a7977298e697eb9f028f522908f12c
Jena Stewart <66694416+jenastewart@users.noreply.github.com>
2024-09-17 15:35:50 -0700

    Added basic JS form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "The poem will go here",
+    autoStart: true,
+    delay: "natural",
+    cursor: null,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
